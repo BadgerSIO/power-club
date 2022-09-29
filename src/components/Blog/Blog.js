9f1297b13045a5c025238dc71ebcecd022087cd2
BadgerSIO <saadibneomar@gmail.com>
2022-09-29 21:16:56 +0600

    responsive update

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -8,8 +8,8 @@ const Blog = () => {
       .then((data) => setQuestions(data));
   }, []);
   return (
-    <div className="sm:px-6 xl:px-20 p-5 md:py-16">
-      <h1 className="text-2xl mb-10 font-bold capitalize ">Our Blog</h1>
+    <div className="sm:px-6 xl:px-20 p-5 md:py-16 lg:w-3/5 sm:mx-auto md:w-3/4 sm:w-[500px]">
+      <h1 className="text-2xl mb-10 font-bold capitalize">Our Blog</h1>
       {questions.map((qa) => (
         <Qa key={qa.id} qa={qa}></Qa>
       ))}
